7ab7ca6fe7690951f61ac34b736b682bae293a23
Damian Poddebniak <poddebniak@fh-muenster.de>
2017-06-14 20:31:54 +0200

    Implemented --ignore-garbage

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate docopt;
-extern crate csv;
 
 use docopt::Docopt;
+use std::{fmt, process};
+use std::ascii::AsciiExt;
 use std::error::Error;
 use std::fs::File;
 use std::io::{Read, Write, stdin, stdout};
-use std::ascii::AsciiExt;
-use std::fmt;
 
 const USAGE: &'static str = "
 hexlify 0.0.1
 
-Performs bytes-to-hex conversion as implemented in Python's binascii.{un,}hexlify. hexlify reads from stdin if <file> is not specified.
+Perform bytes-to-hexstring conversion and vice-versa as implemented
+in Python's binascii.{un,}hexlify. Read from stdin if <file> is \"-\"
+or not specified. Whitespace is ignored during decoding.
 
 Usage: monitor [options] [<file>]
 
 Options:
   -d --decode          Decode stream.
-  -i --ignore-garbage  Ignore bad characters.
+  -i --ignore-garbage  Ignore non-hex values.
   -h --help            Show this help screen.
 ";
 
@@ -27,29 +28,27 @@ Options:
 struct Args {
     arg_file:            Option<String>,
     flag_decode:         bool,
-    flag_ignore_garbage: bool,
+	flag_ignore_garbage: bool,
 }
 
 #[derive(Debug)]
-struct CodingError {
+struct CliError {
 	cause: String
 }
 
-impl CodingError {
-	fn new(cause: &str) -> CodingError {
-		CodingError {
-			cause: cause.into()
-		}
+impl CliError {
+	fn new(cause: &str) -> CliError {
+		CliError { cause: cause.into() }
 	}
 }
 
-impl fmt::Display for CodingError {
+impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.cause)
     }
 }
 
-impl Error for CodingError {
+impl Error for CliError {
     fn description(&self) -> &str {
 		&self.cause
 	}
@@ -59,53 +58,57 @@ impl Error for CodingError {
 	}
 }
 
-fn to_hex_value(c: char) -> Option<u8> {
-	if '0' <= c && c <= '9' { return Some(c as u8 - '0' as u8) }
-	if 'a' <= c && c <= 'f' { return Some(c as u8 - 'a' as u8 + 10) }
-	None
+fn to_hex_value(c: char) -> Result<u8, Box<Error>> {
+	let c = c.to_ascii_lowercase();
+	if '0' <= c && c <= '9' { return Ok(c as u8 - '0' as u8) }
+	if 'a' <= c && c <= 'f' { return Ok(c as u8 - 'a' as u8 + 10) }
+	Err(Box::new(CliError::new("Each character must match hex alphabet, i.e. a-z, A-Z or 0-9. Use -i to ignore non-hex characters.")))
+}
+
+fn pair_to_hex(pair: &[char; 2]) -> Result<u8, Box<Error>> {
+	let l = to_hex_value(pair[0])?;
+	let r = to_hex_value(pair[1])?;
+	Ok(l*16 + r)
 }
 
 fn run(file: Option<String>, decode: bool, ignore_garbage: bool) -> Result<(), Box<Error>> {
-    let mut src: Box<Read> = match file {
-        Some(path) => Box::new(File::open(path)?),
-        None => Box::new(stdin()),
-    };
-
-	if ignore_garbage {
-		unimplemented!();
-	}
+	let src: Box<Read> = match file {
+		Some(path) => {
+			if path != "-" { 
+				Box::new(File::open(path)?)
+			} else {
+				Box::new(stdin())
+			}
+		}
+		None => Box::new(stdin()),
+	};
 
 	if decode {
-		loop {
-			let mut buffer = [0u8; 2];
-			let pair = src.read(&mut buffer);
-			let (l, r) = match pair {
-				Ok(2) => (buffer[0], buffer[1]),
-				Ok(1) => return Err(Box::new(CodingError::new("Input stream must have even number of characters."))),
-				Ok(0) => return Ok(()),
-				Ok(_) => unreachable!(),			
-				Err(err) => return Err(Box::new(err)),
-			};
+		let mut side = 0;
+		let mut pair = [0 as char; 2];
 
-			let l = (l as char).to_ascii_lowercase();
-			let r = (r as char).to_ascii_lowercase();
-		
-			if let Some(l) = to_hex_value(l) {
-				if let Some(r) = to_hex_value(r) {
-					stdout().write_all(&[l * 16 + r])?;
-					continue;
-				}
+		for byte in src.bytes() {
+			let byte = byte? as char;
+			if byte.is_whitespace() { continue }
+			if ignore_garbage && to_hex_value(byte).is_err() { continue }
+
+			pair[side] = byte;
+
+			if side == 0 {
+				side = 1;
+			} else {
+				stdout().write(&[pair_to_hex(&pair)?])?;
+				side = 0;
 			}
-
-			return Err(Box::new(CodingError::new("Input stream must match hex alphabet, i.e. [a-zA-Z0-9].")));
+		}
+		
+		if side == 1 {
+			return Err(Box::new(CliError::new("Input stream must contain even number of characters. The last incomplete hex-pair was omitted.")))
 		}
 	} else {
 		for byte in src.bytes() {
-			match byte {
-				Ok(byte) => print!("{:02X}", byte),
-				Err(err) => return Err(Box::new(err)),
-			}
-		};
+			print!("{:02X}", byte?);
+		}
 	}
 
 	Ok(())
@@ -117,5 +120,8 @@ fn main() {
         .unwrap_or_else(|e| e.exit());
 
     run(args.arg_file, args.flag_decode, args.flag_ignore_garbage)
-		.unwrap_or_else(|e| writeln!(&mut std::io::stderr(), "{}", e).unwrap());
+		.unwrap_or_else(|e| {
+			writeln!(&mut std::io::stderr(), "{}", e).unwrap();
+			process::exit(1);
+		});
 }
