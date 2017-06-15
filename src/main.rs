290674e541035864d42d2bf4f15781356c9132b8
Damian Poddebniak <poddebniak@fh-muenster.de>
2017-06-15 12:32:59 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,20 @@ Options:
 
 #[derive(Debug, Deserialize)]
 struct Args {
-    arg_file:            Option<String>,
-    flag_decode:         bool,
-	flag_ignore_garbage: bool,
+    arg_file: Option<String>,
+    flag_decode: bool,
+    flag_ignore_garbage: bool,
 }
 
 #[derive(Debug)]
 struct CliError {
-	cause: String
+    cause: String,
 }
 
 impl CliError {
-	fn new(cause: &str) -> CliError {
-		CliError { cause: cause.into() }
-	}
+    fn new(cause: &str) -> CliError {
+        CliError { cause: cause.into() }
+    }
 }
 
 impl fmt::Display for CliError {
@@ -50,68 +50,76 @@ impl fmt::Display for CliError {
 
 impl Error for CliError {
     fn description(&self) -> &str {
-		&self.cause
-	}
+        &self.cause
+    }
 
     fn cause(&self) -> Option<&Error> {
-		None	
-	}
+        None
+    }
 }
 
 fn to_hex_value(c: char) -> Result<u8, Box<Error>> {
-	let c = c.to_ascii_lowercase();
-	if '0' <= c && c <= '9' { return Ok(c as u8 - '0' as u8) }
-	if 'a' <= c && c <= 'f' { return Ok(c as u8 - 'a' as u8 + 10) }
-	Err(Box::new(CliError::new("Each character must match hex alphabet, i.e. a-z, A-Z or 0-9. Use -i to ignore non-hex characters.")))
+    let c = c.to_ascii_lowercase();
+    if '0' <= c && c <= '9' {
+        return Ok(c as u8 - '0' as u8);
+    }
+    if 'a' <= c && c <= 'f' {
+        return Ok(c as u8 - 'a' as u8 + 10);
+    }
+    Err(Box::new(CliError::new("Each character must match hex alphabet, i.e. a-z, A-Z or 0-9. Use -i to ignore non-hex characters.",),),)
 }
 
 fn pair_to_hex(pair: &[char; 2]) -> Result<u8, Box<Error>> {
-	let l = to_hex_value(pair[0])?;
-	let r = to_hex_value(pair[1])?;
-	Ok(l*16 + r)
+    let l = to_hex_value(pair[0])?;
+    let r = to_hex_value(pair[1])?;
+    Ok(l * 16 + r)
 }
 
 fn run(file: Option<String>, decode: bool, ignore_garbage: bool) -> Result<(), Box<Error>> {
-	let src: Box<Read> = match file {
-		Some(path) => {
-			if path != "-" { 
-				Box::new(File::open(path)?)
-			} else {
-				Box::new(stdin())
-			}
-		}
-		None => Box::new(stdin()),
-	};
+    let src: Box<Read> = match file {
+        Some(path) => {
+            if path != "-" {
+                Box::new(File::open(path)?)
+            } else {
+                Box::new(stdin())
+            }
+        }
+        None => Box::new(stdin()),
+    };
 
-	if decode {
-		let mut side = 0;
-		let mut pair = [0 as char; 2];
+    if decode {
+        let mut side = 0;
+        let mut pair = [0 as char; 2];
 
-		for byte in src.bytes() {
-			let byte = byte? as char;
-			if byte.is_whitespace() { continue }
-			if ignore_garbage && to_hex_value(byte).is_err() { continue }
+        for byte in src.bytes() {
+            let byte = byte? as char;
+            if byte.is_whitespace() {
+                continue;
+            }
+            if ignore_garbage && to_hex_value(byte).is_err() {
+                continue;
+            }
 
-			pair[side] = byte;
+            pair[side] = byte;
 
-			if side == 0 {
-				side = 1;
-			} else {
-				stdout().write(&[pair_to_hex(&pair)?])?;
-				side = 0;
-			}
-		}
-		
-		if side == 1 {
-			return Err(Box::new(CliError::new("Input stream must contain even number of characters. The last incomplete hex-pair was omitted.")))
-		}
-	} else {
-		for byte in src.bytes() {
-			print!("{:02X}", byte?);
-		}
-	}
+            if side == 0 {
+                side = 1;
+            } else {
+                stdout().write(&[pair_to_hex(&pair)?])?;
+                side = 0;
+            }
+        }
 
-	Ok(())
+        if side == 1 {
+            return Err(Box::new(CliError::new("Input stream must contain even number of characters. The last incomplete hex-pair was omitted.",),),);
+        }
+    } else {
+        for byte in src.bytes() {
+            print!("{:02X}", byte?);
+        }
+    }
+
+    Ok(())
 }
 
 fn main() {
@@ -119,9 +127,8 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    run(args.arg_file, args.flag_decode, args.flag_ignore_garbage)
-		.unwrap_or_else(|e| {
-			writeln!(&mut std::io::stderr(), "{}", e).unwrap();
-			process::exit(1);
-		});
+    run(args.arg_file, args.flag_decode, args.flag_ignore_garbage).unwrap_or_else(|e| {
+        writeln!(&mut std::io::stderr(), "{}", e).unwrap();
+        process::exit(1);
+    });
 }
