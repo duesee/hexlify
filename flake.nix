43dc38a8854272d046ffa739aa4fd9d31345084f
Damian Poddebniak <poddebniak@mailbox.org>
2025-05-17 15:36:37 +0200

    build: Support Nix

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    # The rustup equivalent for Nix.
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Allows non-flakes users to still be able to `nix-shell` based on
+    # `shell.nix` instead of this `flake.nix`.
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      eachSupportedSystem = lib.genAttrs supportedSystems;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      mkDevShells =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          # get the rust toolchain from the rustup
+          # `rust-toolchain.toml` configuration file
+          rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
+          };
+
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ rust-toolchain ];
+          };
+        };
+
+    in
+    {
+      devShells = eachSupportedSystem mkDevShells;
+    };
+}
