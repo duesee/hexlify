f83652e16cdb04deed08d1b46f2613c275393f4e
Damian Poddebniak <poddebniak@mailbox.org>
2025-05-17 18:56:13 +0200

    build: add default.nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
+pkgs.rustPlatform.buildRustPackage {
+  pname = manifest.name;
+  version = manifest.version;
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+  postInstall = ''
+    ln -s $out/bin/hexlify $out/bin/unhexlify
+  '';
+}
